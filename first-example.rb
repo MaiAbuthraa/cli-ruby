8d35ad9f1829ec1a7122fe3cddeabb72f68a5a62
Mai Abuthraa <maiabuthraa@mais-mbp.mynet>
2020-11-19 22:52:39 +0200

    Start use ARGV

---
+++ first-example.rb
@@ -0,0 +1,5 @@
+# Run: ruby first-example.rb 1 2 mai
+# Outout: ["1", "2", "Mai"]
+
+p ARGV
+# ARGF is a stream designed for use in scripts that process files given as command-line arguments or passed in via STDIN
