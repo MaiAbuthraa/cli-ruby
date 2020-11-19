8e9acd14af9e4368e4cfadf716cbd32f2259b325
Mai Abuthraa <maiabuthraa@mais-mbp.mynet>
2020-11-19 22:52:59 +0200

    Start use ARGV / build menu

---
+++ second-example.rb
@@ -0,0 +1,17 @@
+def menu(option)
+  case option
+  when "-h"
+    puts "This is the help menu."
+    puts "-v | get vesrion"
+    puts "-s | print setting "
+  when "-v"
+    puts "1.2.3.4"
+  when "-s"
+    puts "db: database name"
+    puts "host: localhost"
+  end
+end
+
+@option = {}
+
+ARGV.each{|option| menu(option) }
